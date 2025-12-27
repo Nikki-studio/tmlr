4c571d8ea52cbdb74c6b866479c0abca67ce741f
Nikki-studio <joewamwaniki@gmail.com>
2025-12-27 15:08:11 +0300

    added my email📬 and my name to the source⅏⅏ and header files and the makefile 🥰🥰🥰🥳🥳

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # TMLR
 
 ```ascii
-Copyright (C) 2025 Joseph Wangai Mwaniki
+ *  Copyright (C) 2024  Joseph Wangai Mwaniki joewamwaniki@gmail.com
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
--- lexer.hpp
+++ lexer.hpp
@@ -1,5 +1,5 @@
 /** lexer.hpp
- *  Copyright (C) 2024  Your Name <your.email@example.com>
+ *  Copyright (C) 2024  Joseph Wangai Mwaniki joewamwaniki@gmail.com
  *
  *  This program is free software: you can redistribute it and/or modify
  *  it under the terms of the GNU General Public License as published by
